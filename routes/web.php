deb094886df8b47947349b07bbdfc73ab1d69a90
Jessica <jesscara.993@gmail.com>
2023-02-06 16:40:52 +0100

    defined controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MainController;
+
 Route::get('/', function () {
   return view('home');
 });
+
+Route::get('/', [MainController::class, 'home']);
